a37f5497f6a65a16df8a75782153e4df319c5088
Gilbert Dannelle Lo <gilbertdannellelo@gmail.com>
2024-10-25 13:59:08 +0700

    fix(main): added __init__.__all__
    to make flake8 happy

--- tbgclient/__init__.py
+++ tbgclient/__init__.py
@@ -5,5 +5,9 @@ Provides a way to get, post, and modify posts on the TBGs.
 from . import api
 from . import exceptions
 
-from .forum import *
+from .forum import Message, Topic, User, Page
 from .session import Session
+
+__all__ = [
+    "api", "exceptions", "Message", "Topic", "User", "Page", "Session"
+]
