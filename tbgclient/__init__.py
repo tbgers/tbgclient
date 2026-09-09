6e25e798069037621f4622bd635d9ce0db78609b
Gilbert Dannelle Lo <gilbertdannellelo@gmail.com>
2026-09-10 02:53:59 +0700

    fix: don't use the method= arg for the convenience functions

--- tbgclient/__init__.py
+++ tbgclient/__init__.py
@@ -20,46 +20,60 @@ def get_message(mid: int, method: str = "get") -> Message:
     """Gets a message with the specified message ID.
 
     :param mid: The message ID.
-    :param method: The method to use. See
-                   :py:class:`tbgclient.forum.Message`.
+    :param method: The method to use. This should be "get" or "quotefast".
     """
-    return Message(mid=mid).update(method=method)
+    msg = Message(mid=mid)
+    if method == "get":
+        return msg.update_get()
+    elif method == "quotefast":
+        return msg.update_quotefast()
+    else:
+        raise ValueError("method should be either 'get' or 'quotefast'")
 
 
-def get_topic(tid: int, method: str = "get") -> Topic:
+def get_topic(tid: int) -> Topic:
     """Gets a topic with the specified topic ID.
 
     :param tid: The topic ID.
-    :param method: The method to use. See
-                   :py:class:`tbgclient.forum.Topic`.
     """
     from .forum import Topic
-    return Topic(tid=tid).update(method=method)
+    return Topic(tid=tid).update()
 
 
-def get_user(uid: int, method: str = "get") -> User:
+def get_user(uid: int) -> User:
     """Gets a user with the specified user ID.
 
     :param uid: The user ID.
-    :param method: The method to use. See
-                   :py:class:`tbgclient.forum.User`.
     """
-    return User(uid=uid).update(method=method)
+    return User(uid=uid).update()
 
 
-def post_message(tid: int, subject: str, content: str, method: str = "post",
-                 **kwargs) -> Message:
+def post_message(tid: int, subject: str, content: str, **kwargs) -> Message:
     """Post a message with the specified topic ID.
     For other keyword arguments, see :py:class:`tbgclient.forum.Message`.
 
     :param tid: The destination topic ID.
     :param subject: The subject of the message.
     :param content: The content of the message.
-    :param method: The method to use. See
-                   :py:class:`tbgclient.forum.Message`."""
+    """
     return (
-        Message(tid, subject=subject, content=content, **kwargs)
-        .submit(method=method)
+        Message(tid=tid, subject=subject, content=content, **kwargs)
+        .submit_post()
+    )
+
+
+def edit_message(mid: int, tid: int, subject: str, content: str,
+                 **kwargs) -> Message:
+    """Edit a message with the specified topic ID and message ID.
+    For other keyword arguments, see :py:class:`tbgclient.forum.Message`.
+
+    :param tid: The destination topic ID.
+    :param subject: The subject of the message.
+    :param content: The content of the message.
+    """
+    return (
+        Message(mid=mid, tid=tid, subject=subject, content=content, **kwargs)
+        .submit_edit()
     )
 
 
